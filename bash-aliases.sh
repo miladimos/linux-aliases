5e10bc0058759eaa643400fbb9dbe2d2be4a41dc
milad.jafari6210@gmail.com <milad.jafari6210@gmail.com>
2021-03-10 02:01:01 +0330

    add some alises

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -1,1 +1,25 @@
 alias sail='bash vendor/bin/sail'
+alias cls='clear'
+alias llt='ls -ltrh'
+alias lla='ls -la'
+
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+alias sha1='openssl sha1'
+
+alias h='history'
+alias j='jobs -l'
+
+alias ip='sudo ifconfig'
+
+alias reboot='sudo /sbin/reboot'
+alias poweroff='sudo /sbin/poweroff'
+
+alias ff4='/opt/firefox4/firefox'
+alias chrome='/opt/google/chrome/chrome'
+alias opera='/opt/opera/opera'
